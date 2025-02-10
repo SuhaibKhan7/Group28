a405dcdd67883e40507468170c016a9f6fd4c7f3
suhaibkhan <johnxuhaib@gmail.com>
2025-02-10 15:03:15 +0530

    07-02-2025 Lec1

---
+++ 1-Graphql/db.js
@@ -0,0 +1,9 @@
+const users = [
+  { id: 1, name: "John", email: "john@gmail" },
+  { id: 2, name: "Jane", email: "jane@gmail" },
+];
+const reviews = [
+  { id: 1, rating: 4, review: "Good" },
+  { id: 2, rating: 5, review: "Bad" },
+];
+export default { users, reviews };
